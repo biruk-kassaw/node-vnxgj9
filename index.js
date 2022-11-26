721c9c6b1a80a9c83a2e0a119074c6e260ec4b68
biruk-kassaw <61546383+biruk-kassaw@users.noreply.github.com>
2022-11-26 10:26:10 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // run `node index.js` in the terminal
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 console.log(`Hello Node.js v${process.versions.node}!`);
 
 const url = 'https://getbootstrap.com/';
 
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello ma nigga</p>`);
+
+console.log(dom.window.document.querySelector('p').textContent);
